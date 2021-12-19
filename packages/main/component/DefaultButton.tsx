1f8d4c953dfa592afbd6d8a1ef1754be1be4a34d
spearker <scchosam132@gmail.com>
2021-12-19 19:11:38 +0900

    수정

--- packages/main/component/DefaultButton.tsx
+++ packages/main/component/DefaultButton.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useRouter } from 'next/router'
-import styles from '../styles/Home.module.css'
 import {PointColorButton} from '../styles/styledComponents'
 
 interface IProps{
