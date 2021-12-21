7c7fa4ae0c9c17347efd84850829f00edb6c94f8
YunGyeongHyeon <ru9508@naver.com>
2021-12-21 12:09:46 +0900

    제품등록 저장 에러 수정

--- packages/basic/src/container/BasicProduct.tsx
+++ packages/basic/src/container/BasicProduct.tsx
@@ -178,7 +178,7 @@ const BasicProduct = ({page, keyword, option}: IProps) => {
               customer_id: row.customerArray.customer_id,
               model: row.modelArray,
               standard_uph: row.uph,
-              type:row.typePK,
+              type:row.type_id,
               additional: [
                 ...additional.map((v, index)=>{
                   if(!row[v.colName]) return undefined;
