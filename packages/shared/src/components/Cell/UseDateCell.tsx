cfdd6cd14ddbb164614360723f1cbf74f7f2d80a
spearker <scchosam132@gmail.com>
2021-12-30 14:12:06 +0900

    기준일 0일때 경과일에 빨간색 표시 없앰

--- packages/shared/src/components/Cell/UseDateCell.tsx
+++ packages/shared/src/components/Cell/UseDateCell.tsx
@@ -31,7 +31,7 @@ const UseDateCell = ({ row, column, onRowChange }: IProps) => {
 
     const elapsedDay = Math.floor(Number(moment.duration(now.diff(useDate)).asDays()))
 
-    if( row.date && elapsedDay >= row.expiration ){
+    if( row.date && row.expiration && elapsedDay >= row.expiration ){
       setBgColor('red')
     }else{
       setBgColor(undefined)
