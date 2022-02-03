d28ac94fea44effae07e2e1bf27cbe7516b667a5
MacBook_Pro <jmyoon555@gmail.com>
2022-02-03 19:04:45 +0900

    polyfill main index.tsx로 변경

--- packages/main/src/index.tsx
+++ packages/main/src/index.tsx
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 //기준정보관리
 export {BasicFactory} from './container/BasicFactory'
 export {BasicDevice} from './container/BasicDevice'
