9bea9a86eaeccce0ed29f310b49ec4adb0b9d10b
MacBook_Pro <jmyoon555@gmail.com>
2022-02-21 18:47:44 +0900

    작업 일보 등록 -> 금형 보기 추가 하고 등록했을 때 안보이던 현상 수정

--- packages/shared/src/components/Modal/MoldSelectModal.tsx
+++ packages/shared/src/components/Modal/MoldSelectModal.tsx
@@ -50,7 +50,7 @@ const MoldSelectModal = ({column, row, onRowChange}: IProps) => {
   const [optionIndex, setOptionIndex] = useState<number>(0)
   const [keyword, setKeyword] = useState<string>('')
   const [selectRow, setSelectRow] = useState<number>()
-  const [searchList, setSearchList] = useState<any[]>([{seq: 1}])
+  const [searchList, setSearchList] = useState<any[]>([])
   const [searchKeyword, setSearchKeyword] = useState<string>('')
   const [pageInfo, setPageInfo] = useState<{page: number, total: number}>({
     page: 1,
@@ -174,6 +174,7 @@ const MoldSelectModal = ({column, row, onRowChange}: IProps) => {
 
   const updateMold = (moldInfo) => {
     const moldUpdateList = moldInfo.map((v)=>{return v.mold})
+
     setSearchList(moldUpdateList)
   }
 
@@ -247,7 +248,7 @@ const MoldSelectModal = ({column, row, onRowChange}: IProps) => {
           <div style={{display: 'flex', justifyContent: 'space-between', height: 64}}>
             <div style={{height: '100%', display: 'flex', alignItems: 'flex-end', paddingLeft: 16,}}>
               <div style={{ display: 'flex', width: 1200}}>
-                <p style={{fontSize: 22, padding: 0, margin: 0}}>선택 가능 금형 리스트 (여려금형을 동시에 선택하여 사용할 수 있습니다)</p>
+                <p style={{fontSize: 22, padding: 0, margin: 0}}>선택 가능 금형 리스트 (여러금형을 동시에 선택하여 사용할 수 있습니다)</p>
               </div>
             </div>
             <div style={{display: 'flex', justifyContent: 'flex-end', margin: '24px 48px 8px 0'}}>
