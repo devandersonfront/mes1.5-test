3b41290d964dcf4dbb17b4e9cdcd343cc29b71ec
MacBook_Pro <jmyoon555@gmail.com>
2022-02-24 11:54:43 +0900

    이력보기, 총 카운터+ 총 양품 수량 + 총 불량 수량 바인딩 수정 완료

--- packages/shared/src/components/Modal/WorkListModal.tsx
+++ packages/shared/src/components/Modal/WorkListModal.tsx
@@ -64,13 +64,13 @@ const WorkListModal = ({column, row, onRowChange}: IProps) => {
   const [focusIndex, setFocusIndex] = useState<number>(0)
 
   useEffect(() => {
-    // if(isOpen && row.os_id) {
-      // SearchBasic()
-    // }
-    if(row.os_id){
+    if(isOpen && row.os_id) {
       SearchBasic()
     }
-  }, [row.os_id])
+    // if(row.os_id){
+    //   SearchBasic()
+    // }
+  }, [isOpen,row.os_id])
 
   const changeRow = (tmpRow: any, key?: string) => {
     let tmpRes = []
@@ -150,8 +150,7 @@ const WorkListModal = ({column, row, onRowChange}: IProps) => {
     // setOptionIndex(option)
     const res = await RequestMethod('get', `recordAll`,{
       params: {
-        identification:row.os_id
-        // sheetIds: row.os_id
+        sheetIds: row.os_id
       }
     })
     // const res = await RequestMethod('get', `recordSearch`,{
