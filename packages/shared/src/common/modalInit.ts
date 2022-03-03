e6934a2738c7921638c80da8672b409fc0183a72
yosup <yosup1004@gmail.com>
2022-03-03 17:33:35 +0900

    Update : 작업지시서 등록 자재보기 수정 및 BOM 보기 수정 완료

--- packages/shared/src/common/modalInit.ts
+++ packages/shared/src/common/modalInit.ts
@@ -22,6 +22,7 @@ import OperationSearchModal from "../../../main/component/Modal/OperationSearchM
 import {ToolSelectModal} from "../components/Modal/ToolSelectModal";
 import {ToolListModal} from "../components/Modal/ToolListModal";
 import {LotInputInfoModal} from "../components/Modal/LotInputInfoModal";
+import { BomInfoModal } from '../components/Modal/BomInfoModal'
 
 
 export const searchModalList: any = {
@@ -332,7 +333,7 @@ export const searchModalList: any = {
     {key: 'disturbance', name: '소요량', formatter: LineBorderContainer, textAlign: 'center'},
     {key: 'stock', name: '재고량', formatter: LineBorderContainer, textAlign: 'center'},
     {key: 'process', name: '생산 공정', formatter: LineBorderContainer, textAlign: 'center'},
-    {key: 'bom', name: 'BOM', width: 160, formatter: AddTabButton, placeholder: '-' },
+    {key: 'bom', name: 'BOM', width: 160, formatter: BomInfoModal, placeholder: '-' , type : 'readonly' },
   ],
   InputInfo: [
     {key: 'seq', name: '번호', width: 64, alignText: 'center', formatter: LineBorderContainer, textAlign: 'center'},
