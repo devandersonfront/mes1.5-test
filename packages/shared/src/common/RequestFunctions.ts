18c65dd1c956c8d2dbde06f67bd7670e56652401
yosup <yosup1004@gmail.com>
2022-03-08 10:45:54 +0900

    Update : 500에러에 대한 발리데이션 추가

--- packages/shared/src/common/RequestFunctions.ts
+++ packages/shared/src/common/RequestFunctions.ts
@@ -167,6 +167,10 @@ export const requestApi = async (type: RequestType,url: string, data?: any, toke
               Notiflix.Loading.remove(300)
               Notiflix.Report.failure('삭제할 수 없습니다.', error.response.data.message, '확인', )
               return false
+            }else if (error.response.status === 500){
+              Notiflix.Loading.remove(300)
+              Notiflix.Report.failure('서버 에러', '서버 에러입니다. 관리자에게 문의하세요', '확인')
+              return false
             }
           })
   }
