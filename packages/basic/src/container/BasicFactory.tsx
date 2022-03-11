565ef82521770efa66f9b190b7f3bfdd6728e510
YunGyeongHyeon <ru9508@naver.com>
2022-03-11 22:11:09 +0900

    ExcelDownModal 주석

--- packages/basic/src/container/BasicFactory.tsx
+++ packages/basic/src/container/BasicFactory.tsx
@@ -570,16 +570,16 @@ const BasicFactory = ({}: IProps) => {
                     setPageInfo({...pageInfo,page:page})
                 }}
             />
-            <ExcelDownloadModal
-                isOpen={excelOpen}
-                column={column}
-                basicRow={basicRow}
-                filename={`금형기준정보`}
-                sheetname={`금형기준정보`}
-                selectList={selectList}
-                tab={'ROLE_BASE_07'}
-                setIsOpen={setExcelOpen}
-            />
+            {/*<ExcelDownloadModal*/}
+            {/*    isOpen={excelOpen}*/}
+            {/*    column={column}*/}
+            {/*    basicRow={basicRow}*/}
+            {/*    filename={`금형기준정보`}*/}
+            {/*    sheetname={`금형기준정보`}*/}
+            {/*    selectList={selectList}*/}
+            {/*    tab={'ROLE_BASE_07'}*/}
+            {/*    setIsOpen={setExcelOpen}*/}
+            {/*/>*/}
         </div>
     );
 }
