a38f2b0178b67a221350ff8192ea0168ad0903af
yosup <yosup1004@gmail.com>
2022-03-18 17:50:52 +0900

    Update : 라우터에 프레스 상태 모니터링 추가

--- packages/main/common/menulist.ts
+++ packages/main/common/menulist.ts
@@ -146,7 +146,7 @@ const PMS_MENUS: IMenuType[] = [
     title: '프레스 모니터링', url: '',
     subMenu: [
       { title: '프레스별 분석 모니터링', url: '/pms/v2/factoryMonitoring' },
-      // { title: '프레스 현황 모니터링', url: '/pms/new/monitoring/press' }
+      { title: '프레스 상태 모니터링' , url : '/pms/new/monitoring/press'}
     ]
   },
   {
