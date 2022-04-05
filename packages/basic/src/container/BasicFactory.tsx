9216b2fddac337093bc79b865735a6ada0e0034d
YunGyeongHyeon <ru9508@naver.com>
2022-04-05 11:56:18 +0900

    일상점검 css 수정 및 console.log 제거

--- packages/basic/src/container/BasicFactory.tsx
+++ packages/basic/src/container/BasicFactory.tsx
@@ -171,7 +171,7 @@ const BasicFactory = ({}: IProps) => {
                         })
                         return {
                             ...row,
-                            id: row.tmpId,
+                            // id: row.tmpId,
                             authority: row.authorityPK,
                             manager: row.user,
                             version: row.version ?? null,
@@ -180,7 +180,7 @@ const BasicFactory = ({}: IProps) => {
                                     //if(!row[v.colName]) return undefined;
                                     // result.push(
                                     return {
-                                        mi_id: v.id,
+                                        // mi_id: v.id,
                                         title: v.name,
                                         value: row[v.colName] ?? "",
                                         unit: v.unit,
