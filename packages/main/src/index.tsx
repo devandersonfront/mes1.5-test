3acacfa439abbf9f609b649fe896e4202d3e29a8
amoneynous <chajongh@gmail.com>
2022-04-27 10:28:06 +0900

    delete: 안쓰는 팩토리 컨테이너 삭제

--- packages/main/src/index.tsx
+++ packages/main/src/index.tsx
@@ -2,7 +2,6 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 //기준정보관리
-export {BasicFactory} from './container/BasicFactory'
 export {BasicDevice} from './container/BasicDevice'
 export {BasicMachineV1u} from './container/BasicMachineV1u'
 export {BasicRawMaterial} from './container/BasicRawMaterial'
