1220a22837a3fad7c51c0a8672b2db57e542f01a
JungYOsup <yosup1004@gmail.com>
2022-08-12 17:47:12 +0900

    Update : 업체별 타이틀명 커스텀 #213

---
+++ packages/mes/src/container/Defect/MesDefectList.tsx
@@ -0,0 +1,430 @@
+import React, {useEffect, useState} from 'react'
+import {
+  columnlist,
+  ExcelTable,
+  Header as PageHeader,
+  IExcelHeaderType,
+  MAX_VALUE, PaginationComponent,
+  RequestMethod,
+  TextEditor
+} from 'shared'
+import {NextPageContext} from 'next'
+// @ts-ignore
+import {SelectColumn} from 'react-data-grid'
+import Notiflix from "notiflix";
+import {useRouter} from 'next/router'
+import moment from 'moment'
+import {TransferCodeToValue} from 'shared/src/common/TransferFunction'
+import {useDispatch} from 'react-redux'
+import { getTableSortingOptions, setExcelTableHeight } from 'shared/src/common/Util'
+import { setModifyInitData } from 'shared/src/reducer/modifyInfo'
+import {deleteMenuSelectState, setMenuSelectState} from "shared/src/reducer/menuSelectState";
+import { TableSortingOptionType } from 'shared/src/@types/type'
+
+interface IProps {
+  children?: any
+  page?: number
+  search?: string
+  option?: number
+}
+
+const optionList = ['지시 고유 번호', '거래처명', '모델', 'CODE', '품명']
+
+const MesDefectList = ({page, search, option}: IProps) => {
+  const router = useRouter()
+  const dispatch = useDispatch()
+
+  const [basicRow, setBasicRow] = useState<Array<any>>([])
+  const [column, setColumn] = useState<Array<IExcelHeaderType>>( columnlist["operationListV2"])
+  const [selectList, setSelectList] = useState<Set<number>>(new Set())
+  const [optionIndex, setOptionIndex] = useState<number>(0)
+  const [sortingOptions, setSortingOptions] = useState<{orders:string[], sorts:string[]}>({orders:[], sorts:[]})
+  const [selectDate, setSelectDate] = useState<{from:string, to:string}>({
+    from: moment().subtract(1,'month').format('YYYY-MM-DD'),
+    to: moment().format('YYYY-MM-DD')
+  })
+  const [keyword, setKeyword] = useState<string>("");
+  const [pageInfo, setPageInfo] = useState<{page: number, total: number}>({
+    page: 1,
+    total: 1
+  })
+
+  const onSelectDate = (date: {from:string, to:string}) => {
+    setSelectDate(date)
+    reload(null, date)
+  }
+
+  const reload = (keyword?:string, date?:{from:string, to:string}, sortingOptions?: TableSortingOptionType) => {
+    setKeyword(keyword)
+    if(pageInfo.page > 1) {
+      setPageInfo({...pageInfo, page: 1})
+    } else {
+      getData(undefined, keyword, date, sortingOptions)
+    }
+  }
+
+  useEffect(() => {
+    getData(pageInfo.page, keyword)
+  }, [pageInfo.page]);
+
+  useEffect(() => {
+    dispatch(setMenuSelectState({main:"생산관리 등록",sub:router.pathname}))
+    return(() => {
+      dispatch(deleteMenuSelectState())
+    })
+  },[])
+
+  const loadAllSelectItems = async (column: IExcelHeaderType[], date?: {from:string, to:string}) => {
+    const changeOrder = (sort:string, order:string) => {
+      const _sortingOptions = getTableSortingOptions(sort, order, sortingOptions)
+      setSortingOptions(_sortingOptions)
+      reload(null, date, _sortingOptions)
+    }
+    let tmpColumn = column.map((v: any) => {
+      const sortIndex = sortingOptions.sorts.findIndex(value => value === v.key)
+      return {
+        ...v,
+        pk: v.unit_id,
+        sortOption: sortIndex !== -1 ? sortingOptions.orders[sortIndex] : v.sortOption ?? null,
+        sorts: v.sorts ? sortingOptions : null,
+        result: v.sortOption ? changeOrder : null,
+      }
+    });
+
+    setColumn(tmpColumn);
+  }
+
+  const getRequestParams = (keyword?: string, date?: {from:string, to:string},  _sortingOptions?: TableSortingOptionType) => {
+    let params = {}
+    if(keyword) {
+      params['keyword'] = keyword
+      params['opt'] = optionIndex
+    }
+    params['from'] = date ? date.from: selectDate.from
+    params['to'] = date ? date.to : selectDate.to
+    params['order'] = _sortingOptions ? _sortingOptions.orders : sortingOptions.orders
+    params['sorts'] = _sortingOptions ? _sortingOptions.sorts : sortingOptions.sorts
+    params['status'] = '0,1'
+    return params
+  }
+
+  const getData = async (page: number = 1, keyword?: string, date?: {from:string, to:string}, _sortingOptions?: TableSortingOptionType) => {
+    Notiflix.Loading.circle();
+    const res = await RequestMethod("get", keyword ? 'sheetSearch' : 'sheetList', {
+      path: {
+        page: page,
+        renderItem: 18,
+      },
+      params: getRequestParams(keyword, date, _sortingOptions)
+    });
+    if(res){
+      if (res.totalPages > 0 && res.totalPages < res.page) {
+        reload();
+      } else {
+        setPageInfo({
+          page: res.page,
+          total: res.totalPages
+        })
+        cleanUpData(res, date)
+      }
+    }
+    Notiflix.Loading.remove()
+  };
+
+
+  const DeleteBasic = async () => {
+    const res = await RequestMethod('delete', `sheetDelete`,
+      basicRow.map((row, i) => {
+        if(selectList.has(row.id)){
+          let selectKey: string[] = []
+          let additional:any[] = []
+          column.map((v) => {
+            if(v.selectList){
+              selectKey.push(v.key)
+            }
+
+            if(v.type === 'additional'){
+              additional.push(v)
+            }
+          })
+
+          let selectData: any = {}
+
+          Object.keys(row).map(v => {
+            if(v.indexOf('PK') !== -1) {
+              selectData = {
+                ...selectData,
+                [v.split('PK')[0]]: row[v]
+              }
+            }
+
+            if(v === 'unitWeight') {
+              selectData = {
+                ...selectData,
+                unitWeight: Number(row['unitWeight'])
+              }
+            }
+
+            if(v === 'tmpId') {
+              selectData = {
+                ...selectData,
+                id: row['tmpId']
+              }
+            }
+          })
+          return {
+            ...row,
+            ...selectData,
+            type: row.type_id,
+            status: row.status_no,
+            additional: [
+              ...additional.map(v => {
+                if(row[v.name]) {
+                  return {
+                    id: v.id,
+                    title: v.name,
+                    value: row[v.name],
+                    unit: v.unit
+                  }
+                }
+              }).filter((v) => v)
+            ]
+          }
+
+        }
+      }).filter((v) => v))
+
+    if(res) {
+      Notiflix.Report.success('삭제 성공!', '', '확인', () => reload())
+    }
+  }
+
+  const cleanUpData = (res: any, date?: {from:string, to:string}) => {
+    let tmpColumn = columnlist["operationListV2"];
+    let tmpRow = []
+    tmpColumn = tmpColumn.map((column: any) => {
+      let menuData: object | undefined;
+      res.menus && res.menus.map((menu: any) => {
+        if(menu.colName === column.key){
+          menuData = {
+            id: menu.id,
+            name: menu.title,
+            width: menu.width,
+            tab:menu.tab,
+            unit:menu.unit
+          }
+        } else if(menu.colName === 'id' && column.key === 'tmpId'){
+          menuData = {
+            id: menu.id,
+            name: menu.title,
+            width: menu.width,
+            tab:menu.tab,
+            unit:menu.unit
+          }
+        }
+      })
+
+      if(menuData){
+        return {
+          ...column,
+          ...menuData,
+        }
+      }
+    }).filter((v:any) => v)
+
+    let additionalMenus = res.menus ? res.menus.map((menu:any) => {
+      if(menu.colName === null){
+        return {
+          id: menu.id,
+          name: menu.title,
+          width: menu.width,
+          key: menu.title,
+          editor: TextEditor,
+          type: 'additional',
+          unit: menu.unit
+        }
+      }
+    }).filter((v: any) => v) : []
+
+      tmpRow = res.info_list
+
+    loadAllSelectItems( [
+      ...tmpColumn,
+      ...additionalMenus
+    ], date)
+
+
+    let selectKey = ""
+    let additionalData: any[] = []
+    tmpColumn.map((v: any) => {
+      if(v.selectList){
+        selectKey = v.key
+      }
+    })
+
+    additionalMenus.map((v: any) => {
+      if(v.type === 'additional'){
+        additionalData.push(v.key)
+      }
+    })
+
+    let pk = "";
+    Object.keys(tmpRow).map((v) => {
+      if(v.indexOf('_id') !== -1){
+        pk = v
+      }
+    })
+
+    let tmpBasicRow = tmpRow.map((row: any, index: number) => {
+
+      let appendAdditional: any = {}
+
+      row.additional && row.additional.map((v: any) => {
+        appendAdditional = {
+          ...appendAdditional,
+          [v.title]: v.value
+        }
+      })
+
+      let random_id = Math.random()*1000;
+      return {
+        ...row,
+        ...appendAdditional,
+        status: TransferCodeToValue(row.status, 'workStatus'),
+        status_no: row.status,
+        contract_id: row.contract?.identification ?? '-' ,
+        bom_root_id: row.product?.bom_root_id,
+        total_counter: row.total_good_quantity+row.total_poor_quantity,
+        customer_id: row.product.customer?.name ?? '-',
+        cm_id: row.product.model?.model ?? '-',
+        product_id: row.product.code ?? '-',
+        code: row.product.code ?? '-',
+        name: row.product.name ?? '-',
+        type: TransferCodeToValue(row.product.type, 'product'),
+        unit: row.product?.unit ?? '-',
+        process_id: row.product?.process?.name ?? '-',
+        id: `sheet_${random_id}`,
+      }
+    })
+    setSelectList(new Set)
+    setBasicRow([...tmpBasicRow])
+  }
+
+  return (
+    <div>
+      <PageHeader
+        isSearch
+        isCalendar
+        searchOptionList={optionList}
+        optionIndex={optionIndex}
+        calendarTitle={'작업 기한'}
+        calendarType={'period'}
+        selectDate={selectDate}
+        searchKeyword={keyword}
+        onSearch={reload}
+        onChangeSearchOption={(option) => {
+          setOptionIndex(option)
+        }}
+        //@ts-ignore
+        setSelectDate={onSelectDate}
+        //실제사용
+        title={"불량 이력 관리"}
+        buttons={
+          ['', '수정하기', '삭제']
+        }
+        buttonsOnclick={
+          (e) => {
+            switch(e) {
+              case 1:
+                if(selectList.size === 0){
+                  Notiflix.Report.warning("경고","데이터를 선택해주시기 바랍니다.","확인");
+                }else if(selectList.size === 1){
+                  dispatch(setModifyInitData({
+                    modifyInfo: basicRow.map(v => {
+                      if (selectList.has(v.id)) {
+                        return v
+                      }
+                    }).filter(v => v),
+                    type: 'order'
+                  }))
+                  router.push('/mes/defect/modify')
+                }else{
+                  Notiflix.Report.warning("경고","데이터를 하나만 선택해주시기 바랍니다.","확인");
+                }
+                break;
+              case 2:
+                if(selectList.size <= 0) {
+                  return  Notiflix.Report.warning("경고","데이터를 선택해 주시기 바랍니다.","확인" )
+                }
+                Notiflix.Confirm.show("경고","삭제하시겠습니까?","확인","취소",
+                  ()=>{
+                    DeleteBasic()
+                  },
+                  ()=>{}
+                )
+                break;
+            }
+
+          }
+          // onClickHeaderButton
+        }
+      />
+    <ExcelTable
+      editable
+      resizable
+      selectable
+      headerList={[
+        SelectColumn,
+        ...column
+      ]}
+      row={basicRow}
+      // setRow={setBasicRow}
+      setRow={(e) => {
+        let tmp: Set<any> = selectList
+        let tmpRes = e.map(v => {
+          if(v.isChange) {
+                            tmp.add(v.id)
+                            v.isChange = false
+                        }
+          if(v.update || v.finish){
+            reload()
+            return {
+              ...v,
+              update: undefined,
+              finish: undefined,
+            }
+          }
+          return { ...v, }
+        })
+
+        setSelectList(tmp)
+        setBasicRow([...tmpRes])
+      }}
+      selectList={selectList}
+      //@ts-ignore
+      setSelectList={setSelectList}
+      width={1576}
+      height={setExcelTableHeight(basicRow.length)}
+    />
+      <PaginationComponent
+          currentPage={pageInfo.page}
+          totalPage={pageInfo.total}
+          setPage={(page) => {
+            setPageInfo({...pageInfo, page: page})
+          }}
+      />
+    </div>
+  );
+}
+
+export const getServerSideProps = (ctx: NextPageContext) => {
+  return {
+    props: {
+      page: ctx.query.page ?? 1,
+      keyword: ctx.query.keyword ?? "",
+      option: ctx.query.opt ?? 0,
+    }
+  }
+}
+
+export {MesDefectList};
---
+++ packages/shared/src/common/menuTitles/index.ts
@@ -0,0 +1,106 @@
+
+function menuLogic(title : string, url : string, subMenu ?: string[]){
+  return {title,url,subMenu}
+}
+
+export const defaultBasicTitles = {
+
+  userAuthMgmt : menuLogic('사용자 권한 관리','' ,['_authMgmt','_userMgmt']),
+  _authMgmt : menuLogic('권한 관리' ,'/mes/basic/authority/member'),
+  _userMgmt : menuLogic('유저 관리' , '/mes/basic/user'),
+
+  factoryInfo : menuLogic('공장 기준정보','/mes/basic/factory'),
+
+  manageMgmt : menuLogic('거래처 관리','/mes/basic/factory', ['_customMgmt','_modelMgmt']),
+  _customMgmt : menuLogic('거래처 정보 관리','/mes/basic/customer'),
+  _modelMgmt : menuLogic('모델 관리', '/mes/basic/customer/model'),
+
+  processMgmt : menuLogic('공정 관리', '',['_typeMgmt','_pauseMgmt']),
+  _typeMgmt : menuLogic('공정 종류 관리','/mes/basic/process'),
+  _pauseMgmt : menuLogic('공정 일시정지 유형 등록','/mes/basic/register/pause'),
+
+  qualityInfo : menuLogic('품질 기준정보','',['_defectReg']),
+  _defectReg : menuLogic('공정별 불량유형 등록','/mes/basic/register/defect'),
+
+  deviceInfo : menuLogic('주변장치 기준정보','/mes/basic/device'),
+  machineInfo : menuLogic('기계 기준정보', '/mes/basic/machine'),
+  moldInfo : menuLogic('금형 기준정보','/mes/basic/moldV1u'),
+  toolInfo : menuLogic('공구 기준정보','/mes/basic/tool'),
+  rawInfo : menuLogic('원자재 기준정보','/mes/basic/rawmaterialV1u'),
+  subInfo : menuLogic('부자재 기준정보','/mes/basic/submaterial'),
+  productMgmt : menuLogic('제품 등록 관리','/mes/basic/productV1u'),
+  documentMgmt : menuLogic('문서 관리','/mes/basic/document'),
+
+}
+
+export const defaultMesTitles = {
+
+  businessMgmt : menuLogic('영업 관리', '',['_orderReg','_orderList','_deliveryReg','_deliveryList']),
+  _orderReg : menuLogic('수주 정보 등록', '/mes/order/register'),
+  _orderList : menuLogic('수주 현황','/mes/order/list'),
+  _deliveryReg : menuLogic('납품 정보 등록','/mes/delivery/register'),
+  _deliveryList : menuLogic('납품 현황','/mes/delivery/list'),
+
+  pmReg : menuLogic('생산관리 등록','',['_opReg','_opList','_opReList','_opComList']),
+  _opReg : menuLogic('작업지시서 등록','/mes/operationV1u/register'),
+  _opList : menuLogic('작업지시서 리스트','/mes/operationV1u/list'),
+  _opReList : menuLogic('작업 일보 리스트','/mes/recordV2/list'),
+  _opComList : menuLogic('작업 완료 리스트','/mes/finishV2/list'),
+
+  rawMgmt : menuLogic('원자재 관리', '' , ['_rawReg','_rawStock']),
+  _rawReg : menuLogic('원자재 입고 등록','/mes/rawmaterialV1u/input'),
+  _rawStock : menuLogic('원자재 재고 현황','/mes/rawmaterialV1u/stock'),
+
+  subMgmt : menuLogic('부자재 관리','',['_subReg','_subStock']),
+  _subReg : menuLogic('부자재 입고 등록','/mes/submaterialV1u/input'),
+  _subStock : menuLogic('부자재 재고 현황','/mes/submaterialV1u/stock'),
+
+  toolMgmt : menuLogic('공구 관리','',['_toolReg','_toolList','_toolStock']),
+  _toolReg : menuLogic('공구 입고 등록','/mes/tool/register'),
+  _toolList : menuLogic('공구 입고 리스트','/mes/tool/warehousinglist'),
+  _toolStock : menuLogic('공구 재고 현황','/mes/tool/list'),
+
+  qualityMgmt : menuLogic('품질 관리','',['_midRangeList','_stdList','_changeNoti','_changeList']),
+  _midRangeList : menuLogic('초ㆍ중ㆍ종 검사 리스트','/mes/quality/midrange/list'),
+  _stdList : menuLogic('작업 표준서 관리','/mes/quality/work/standardlist'),
+  _changeNoti : menuLogic('변경점 정보 등록','/mes/quality/product/change/register'),
+  _changeList : menuLogic('변경점 정보 리스트','/mes/quality/product/change/list'),
+
+  stockMgmt : menuLogic('재고 관리','',['_stockList','_productList','_productListAdmin']),
+  _stockList : menuLogic('재고 현황','/mes/stock/list'),
+  _productList : menuLogic('생산/납품 현황','/mes/stock/productlist'),
+  _productListAdmin : menuLogic('생산/납품 현황(관리자용)','/mes/stock/admin'),
+
+  kpi : menuLogic('KPI','',['_pLeadTime','_manHour','_defect','_oLeadTime','_powerUsage','_uph','_operation']),
+  _pLeadTime : menuLogic('제조리드타임(P)','/mes/kpi/leadtime/manufacture'),
+  _manHour : menuLogic('작업공수(C)','/mes/kpi/manhour'),
+  _defect : menuLogic('품질 불량률(Q)','/mes/kpi/defect'),
+  _oLeadTime : menuLogic('수주/납품 리드타임(D)','/mes/kpi/leadtime/order'),
+  _powerUsage : menuLogic('전력사용량(E)','/mes/kpi/powerusage'),
+  _uph : menuLogic('UPH(P)','/mes/kpi/uph'),
+  _operation : menuLogic('설비가동률(P)','/mes/kpi/operation')
+}
+
+export const bkMesTitles = {
+
+  ...defaultMesTitles,
+  pmReg : menuLogic('생산관리','',['_opReg','_opList','_opReList','_opComList','_opDefect']),
+  _opReg : menuLogic('생산 계획 관리(작업지시서 등록)','/mes/operationV1u/register'),
+  _opList : menuLogic('생산 계획 관리(작업지시서 리스트)','/mes/operationV1u/list'),
+  _opReList : menuLogic('금일 작업 일보 관리(작업 일보 리스트)','/mes/recordV2/list'),
+  _opComList : menuLogic('작업 이력 관리(작업 완료 리스트)','/mes/finishV2/list'),
+  _opDefect : menuLogic('불량 이력 관리','/mes/defect/list'),
+
+  qualityMgmt : menuLogic('품질 관리','',['_midRangeList','_stdList','_changeNoti','_changeList']),
+  _midRangeList : menuLogic('자주 검사 관리(초ㆍ중ㆍ종 검사 리스트)','/mes/quality/midrange/list'),
+  _stdList : menuLogic('작업 표준서 관리','/mes/quality/work/standardlist'),
+  _changeNoti : menuLogic('제품 변경점 관리(변경점 정보 등록)','/mes/quality/product/change/register'),
+  _changeList : menuLogic('제품 변경점 관리(변경점 정보 리스트)','/mes/quality/product/change/list'),
+
+}
+
+
+
+
+
+
