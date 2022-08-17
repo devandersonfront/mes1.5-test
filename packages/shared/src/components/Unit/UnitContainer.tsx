4230de1d20754dd2aae2d4db6810d5225022bbe8
amoneynous <chajongh@gmail.com>
2022-08-17 17:53:38 +0900

    update: feature/#189

--- packages/shared/src/components/Unit/UnitContainer.tsx
+++ packages/shared/src/components/Unit/UnitContainer.tsx
@@ -14,61 +14,12 @@ interface IProps {
 
 const UnitContainer = ({ row, column, setRow }: IProps) => {
   const [title, setTitle] = useState<string>("")
-  const AddComma = (number: number) => {
-    //후방탐색 부정형은 TV, 아이폰에서 현재 지원을 안함
-    // let regexp = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g
-    return number.toLocaleString()
-  }
 
   useEffect(() => {
     let fixNumber: any = Number(row[column.key]);
-    // if(Number.isInteger(fixNumber))
-    // {
-    //     fixNumber = AddComma(fixNumber);
-    // }else if(fixNumber !== undefined){
-    //반올림이 문제가 발생할 수 있음 (toFixed)
-    console.log("row.type : ", row.type);
+
     if (!isNaN(Number(fixNumber))) {
-
-      if (column.toFix) {
-        // fixNumber = new Big(fixNumber)
-
-        if (row.type && row.type == 'COIL') {
-
-          // console.log("hihi ", isInteger(fixNumber));
-          if (isInteger(fixNumber)) {
-            fixNumber = new Number(fixNumber).toFixed(2)
-
-          } else {
-            // console.log("abcd");
-            fixNumber = new Big(fixNumber).toFixed(column.toFix)
-          }
-
-        } else if (row.type == '반제품') {
-          console.log("반제품");
-          if (isInteger(fixNumber)) {
-            fixNumber = new Number(fixNumber).toFixed(column.toFix)
-
-          } else {
-            // console.log("abcd");
-            fixNumber = new Big(fixNumber)
-          }
-        }
-
-        else {
-          console.log("1234");
-          // fixNumber = new Big(fixNumber);
-          if (isInteger(fixNumber)) {
-            fixNumber = new Number(fixNumber).toFixed(column.toFix)
-
-          } else {
-            console.log("abcd");
-            fixNumber = new Big(fixNumber)
-          }
-
-        }
-      }
-      fixNumber = AddComma(fixNumber)
+      fixNumber = fixNumber.toLocaleString(undefined, {minimumFractionDigits: column.toFix ?? undefined,  maximumFractionDigits: column.toFix ?? undefined})
     } else {
       fixNumber = undefined
     }
