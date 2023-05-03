a3c8aa5506e51f7a07985fc8bb3b3442ad7cdaa9
YunGyeongHyeon <ru9508@naver.com>
2023-05-03 14:53:54 +0900

    FIX : AI 생산 제품 현황 예측 품곡 관리 모달 수정
    예측 품목 관리 list를 받는 API를  list -> search로 변경

--- packages/shared/src/components/Modal/AiCompareModal.tsx
+++ packages/shared/src/components/Modal/AiCompareModal.tsx
@@ -33,7 +33,7 @@ const AiCompareModal =  ({row, column, setRow}: IProps) => {
     const [selectRow, setSelectRow] = useState<number>(null)
 
     const [selectState, setSelectState] = useState<"basic" | "sub">("basic")
-    const [searchModalInit, setSearchModalInit] = useState<any>(SearchInit.product)
+    const searchModalInit = ['', '모델', '코드', '품명']
     const [keyword, setKeyword] = useState<string>("")
     const [option, setOption] = useState<number>(0)
     const [pageInfo, setPageInfo] = useState<{page:number, total:number}>({page:1, total:1})
@@ -76,7 +76,7 @@ const AiCompareModal =  ({row, column, setRow}: IProps) => {
     }, [isOpen])
 
     const getProductDatas = (type:"basic" | "sub", scrollEnd?:boolean) => {
-        RequestMethod("get", scrollEnd ? "productSearch" : "productList",{
+        RequestMethod("get", "productSearch",{
             path: {
                 page: scrollEnd ? pageInfo.page : 1,
                 renderItem: 18,
@@ -120,8 +120,8 @@ const AiCompareModal =  ({row, column, setRow}: IProps) => {
                 backgroundColor: '#F4F6FA', border: '0.5px solid #B3B3B3',
                 borderRight: 'none',
             }}>
-                <select key={searchModalInit?.searchFilter[0]}
-                        defaultValue={searchModalInit?.searchFilter[0]}
+                <select key={searchModalInit[0]}
+                        defaultValue={searchModalInit[0]}
                         onChange={(e) => {
                             const option = Number(e.target.value)
                             setOption(option)
@@ -140,7 +140,7 @@ const AiCompareModal =  ({row, column, setRow}: IProps) => {
                 >
 
                     {
-                        searchModalInit && searchModalInit.searchFilter.map((v, i) => {
+                        searchModalInit.map((v, i) => {
                             if(v !== ""){
                                 return (<option key={i.toString()} value={i}>{v}</option>)
                             }
@@ -158,7 +158,6 @@ const AiCompareModal =  ({row, column, setRow}: IProps) => {
                 }}
                 onKeyDown={(e) => {
                     if(e.key === 'Enter'){
-                        console.log("option : ", option, "  keyword : ", keyword)
                         getProductDatas(selectState)
                         // LoadBasic(1);
                     }
@@ -175,7 +174,6 @@ const AiCompareModal =  ({row, column, setRow}: IProps) => {
                 className={'img_wrapper unprintable'}
                 style={{background:"#19B9DF", width:"32px",height:"32px",display:"flex",justifyContent:"center",alignItems:"center", cursor: 'pointer'}}
                 onClick={() => {
-                    console.log("option : ", option, "  keyword : ", keyword)
                     getProductDatas(selectState)
                     // LoadBasic(1);
                 }}
