c60681afa15af81681d3c6c58aa8cb8f62bbfd79
YunGyeongHyeon <ru9508@naver.com>
2023-05-16 19:28:30 +0900

    ADD : 공지사항 url 주소 .env 파일에서 찾도록 수정

--- packages/shared/src/common/menuTitles/index.ts
+++ packages/shared/src/common/menuTitles/index.ts
@@ -13,7 +13,7 @@ export const HomeTitles = (customTarget?:string) => {
   const homeDefault = {
     home : toMenu(titles.home , '', customTarget === 'ai' ? ['_aiProductLog','_productLog', '_notice'] : ['_productLog', '_notice']),
     _productLog : toMenu(titles._productLog, '/mes/home/productLog'),
-    _notice : toMenu(titles._notice, 'https://sizl-dev.notion.site/361da79588284923bad7a4dc4571a8b1'),
+    _notice : toMenu(titles._notice, process.env.NEXT_PUBLIC_SF_ENDPOINT_NOTICE),
   }
   switch (customTarget) {
     case 'ai' : return {
